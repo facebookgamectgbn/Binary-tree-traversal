faa6abc14089bb90c3ee74f6ea1efec090ffa834
facebookgamectgbn <51239352+facebookgamectgbn@users.noreply.github.com>
2019-11-10 12:07:25 +0600

    Add files via upload

---
+++ Binary tree traversal.cpp
@@ -0,0 +1,80 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+
+Node* Insert(Node* parent,int new_data)
+{
+
+
+   if(parent==NULL)
+   {
+       Node* current_node = new Node();
+       current_node->data=new_data;
+       current_node->left=NULL;
+       current_node->right=NULL;
+       return current_node;
+   }
+   else if(new_data > parent->data)
+   {
+       parent->right = Insert(parent->right,new_data);
+
+   }
+   else if(new_data < parent->data)
+   {
+       parent->left = Insert(parent->left,new_data);
+   }
+    return parent;
+}
+
+void print_postorder(Node* node)
+{
+    if(node==NULL)
+        return;
+    print_postorder(node->left);
+    print_postorder(node->right);
+    cout<<node->data<<"  ";
+}
+void print_preorder(Node* node)
+{
+    if(node==NULL)
+        return;
+    cout<<node->data<<"  ";
+    print_postorder(node->left);
+    print_preorder(node->right);
+
+}
+void print_inorder(Node* node)
+{
+    if(node==NULL)
+        return;
+    print_postorder(node->left);
+    cout<<node->data<<"  ";
+    print_inorder(node->right);
+
+}
+int main()
+{
+    Node* root = NULL;
+    root = Insert(root,12);
+    Insert(root,10);
+    Insert(root,37);
+    Insert(root,21);
+    Insert(root,23);
+    Insert(root,22);
+
+
+    print_preorder(root);
+    cout<<endl;
+    print_inorder(root);
+    cout<<endl;
+    print_postorder(root);
+
+}
+
